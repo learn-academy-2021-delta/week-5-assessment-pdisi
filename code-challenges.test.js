87ad78e96ddca73bdf906982ae354e28571f6073
pdisi <pdisidoro@yahoo.com>
2021-10-10 18:49:03 -1000

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,47 +16,141 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-var secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-var secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-var secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe("Converts letters to associated numbers and returns them", () => {
+
+    // a test/it method, nested within the describe block, that in plain words, describes what the function does.
+    it("Takes in a string and converts specific letters to specific numbers", () => {
+
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(input(secretCodeWord1)).toEqual(("L4ck4d41s1c4l"))
+      expect(input(secretCodeWord2)).toEqual(("G0bbl3dyg00k"))
+      expect(input(secretCodeWord3)).toEqual(("3cc3ntr1c"))
+    })
+})
+// // Test Fail: Input is not defined
+
+// // // b) Create the function that makes the test pass.
+
+// // input a string in an arrow function
+// // use replace built in method to swap out letters for numbers
+// // return a new string.replace
+
+const input = (string) => {
+    return string.replace(/[aA]/g, '4').replace(/[eE]/g, '3').replace(/[iI]/g, '1').replace(/[oO]/g, '0')
+ }
+ 
+ var secretCodeWord1 = "Lackadaisical"
+//  // Expected output: "L4ck4d41s1c4l"
+ var secretCodeWord2 = "Gobbledygook"
+//  // Expected output: "G0bbl3dyg00k"
+ var secretCodeWord3 = "Eccentric"
+//  // Expected output: "3cc3ntr1c"
+
+ //Test Pass
+
+ //Notes on process: I am very happy with the results for this one. It took me a while to figure out how to do it in one line, but it was worth it!
 
 
+// // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
-// b) Create the function that makes the test pass.
+// // a) Create a test with expects statement using the variable provided.
 
+describe("Return all words that contain a particular letter", () => {
 
+    // a test/it method, nested within the describe block, that in plain words, describes what the function does.
+    it("Takes in an array and prints the words that contain specific letters", () => {
 
-// --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
-
-// a) Create a test with expects statement using the variable provided.
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(input(arrayOfWords1, letterA)).toEqual((["Apple", "Banana", "Orange"]))
+      expect(input(arrayOfWords2, letterE)).toEqual((["Cherry", "Blueberry", "Peach"]))
+    })
+})
 
 var arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
 var letterA = "a"
-// Expected output: ["Apple", "Banana", "Orange"]
+// // Expected output: ["Apple", "Banana", "Orange"]
 var arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
 var letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+// // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+// //Test Fail: Input is not defined
 
+// // // b) Create the function that makes the test pass.
+// //Create a generic arrow function called input that takes in an array and string
+// //create a variable called output that is equal to filter values from the given array
+// //return the value and use a method called include to find if the given variable letter is in the string
+// //change the string to uppercase to ensure that capital letters are found
+// //return output
 
-// b) Create the function that makes the test pass.
+const input = (array, string) => {
+    let output = array.filter(value => {
+        return value.includes(string) ||
+        value.includes(string.toUpperCase())
+    })
+    return output
+} 
 
+//Test Pass
 
+//Notes on process: Had some difficulty with this one. I knew that I needed to use the filter method, but it took me a while to find the include method that needed to be used in conjunction with it. Had some help from classmates on Slack to get the include method to work properly.
 
-// --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
+// // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
-// a) Create a test with expect statements using the variable provided.
+// // a) Create a test with expect statements using the variable provided.
+
+describe("Take in an array of numbers and say if it is a full house or not", () => {
+
+    // a test/it method, nested within the describe block, that in plain words, describes what the function does.
+    it("Input an array and return if it is true or false", () => {
+
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(input(hand1)).toEqual((true))
+      expect(input(hand2)).toEqual((false))
+      expect(input(hand3)).toEqual((false))
+    })
+})
+
+//Test Fail: Input is not defined
 
 var hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
+// // Expected output: true
 var hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
+// // Expected output: false
 var hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
+// // Expected output: false
 
+// // b) Create the function that makes the test pass.
 
+//Create a generic arrow function called input that takes in an array
+//Run a for loop to look through the indexes of the given arrays
 
-// b) Create the function that makes the test pass.
+//First attempt
+//Realized that I need a way to store my counts, so I am heading back to the good old GOOGLE
+// const input = (array) => {
+//     for(i = 0; i < array.length; i++){
+//         let value = array[i]
+//         if (array.indexOf(value) !== -1) {
+//             return true
+//         }
+//         array.push(value)
+//     }
+//     return false
+// }
+
+//Second attempt
+//The pseudocode above relates to this one as well
+//I have added in the countObj of x to keep track of duplicate values within the array
+//the conditional statement, if, verifies if the stored data within countObj is strictly 3 of a kind and 2 of a kind then returns true or false
+const input = (arr) => {
+    let countObj = {}
+    for(let x of arr){
+      countObj[x] = (countObj[x] || 0) + 1;
+    }
+    let vals = Object.values(countObj);
+    if((vals[0] === 2 && vals[1] === 3) || (vals[1] === 2 && vals[0] === 3)){
+      return true;
+    }
+    return false;
+  }
+
+  //Notes on process: The logic behind this one was not too difficult, but I needed to do some extra research to remember how to store values. I believe we have already done an if statement similar to this one, so that was relatively familiar for me.
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Are (most) Ruby methods accessors or mutators?
 
-  Your answer:
+  Your answer: The most common Ruby methods are accessors. The built in methods usually only access information instead of changing it.
 
-  Researched answer:
+  Researched answer: For example methods such as .length, .first, .last are far more common than methods such as .pop.
 
 
 
 2. What is a block in Ruby?
 
-  Your answer:
+  Your answer: I believe blocks include code that can be called upon. One example would be a "do" block.
 
-  Researched answer:
+  Researched answer: Blocks can also be identified between braces and end statements. Methods are nested within them and can be called upon.
 
 
 
 3. What is a gem?
 
-  Your answer:
+  Your answer: Gem is a library that works specifically for ruby and I believe that is called a relational database. It is comparable to yarn for javascript.
 
-  Researched answer:
+  Researched answer: It is equivalent to a plugin or extension specifically designed for Ruby programming. They are all independent of other systems and act as a crutch to modify the operations within ruby apps.
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: I believe that this is a database that stores information that is similarly related. However, I also think that these are designed to support specific programs.
 
-  Researched answer:
+  Researched answer: Relational databases allow people to access data among multiple databases. They will have extensive amounts of tabs that are capable of relating to eachother. An example of a relational database is SQL.
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: I am not entirely sure on this one, but I believe that a primary key may be referenced to groups of data, or the starting point of a group of data.
 
-  Researched answer:
+  Researched answer: An example of a primary key would be the title of a column in a database that refers to information within it that is capable of coinciding with. Primary keys seem to be unique as they need to be called upon without confusion. They may also be referred to as a primary keyword. Relational databases only have one primary key.
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: The underlying framework that connects HTTP verbs to CRUD actions (create, read, update, delete). When something follows the REST principles it is known as RESTFUL. They are a conventional pattern that is followed when designing routes for the interaction whenever a request is made by a user.
 
-2. JSON:
+2. JSON: It is a programming langauge that utilizes simple english and objects to execute code. It is known as JavaScript Object Notation. It is used to store and transport data.
 
-3. ERB:
+3. ERB: It is a system used to template and embed Ruby code into a text document. It stands for Embedded Ruby and is capable of transforming Ruby code into HTML code for rendering purposes.
 
-4. Params:
+4. Params: Short for parameter. They are values that are used to pass information to methods. This would be the equivalent action of a "get" to "post" in Ruby.
 
-5. API:
+5. API: Application Programming Interface. It is a set of protocols and definitions for building and integrating softare applications. "APIs are sometimes thought of as contracts, with documentation that represents an agreement between parties: If party 1 sends a remote request structured a particular way, this is how party 2’s software will respond."
